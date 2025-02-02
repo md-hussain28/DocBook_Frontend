310923bb86e415ac099bcbc331cdb8e40c888562
Md Saquib Hussain <mdsaquib28official@gmail.com>
2025-02-03 04:12:20 +0530

    "Error handling 1.2"

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename="/app">
+  <BrowserRouter>
     <App />
   </BrowserRouter>,
 );
